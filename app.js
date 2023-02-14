16a86a825f06816e175bb9585454c7e789f2a476
rohitGupta2022 <pta.rohit28@gmail.com>
2023-02-14 23:25:54 +0530

    updated Cors

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
-app.use(cors());
 
 // 1) MIDDLEWARES
 // Set security http headers
@@ -103,7 +102,18 @@ app.use((req, res, next) => {
 //   res.status(200).send('Hello From the server Side!!');
 // });
 // // app.use('', )
-
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
